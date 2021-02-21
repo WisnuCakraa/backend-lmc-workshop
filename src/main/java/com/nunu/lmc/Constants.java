e9de3a808c5a185ae342029a9884ef274bb45550
HelenF72 <helenfy.72@gmail.com>
2021-02-21 16:30:24 +0700

    [Hell] Add new service user

--- src/main/java/com/nunu/lmc/Constants.java
+++ src/main/java/com/nunu/lmc/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
 
     public static final String COMMON_ERROR_CODE = "X06"; //error which not handle
     public static final String ID_NOT_EXIST_ERROR_CODE = "EX01";
+    public static final String USERNAME_EXIST_ERROR_CODE = "US01";
 
 
 }
